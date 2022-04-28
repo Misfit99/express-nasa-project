baf8394f25496d1fd776dcf118079fa0d7ae3761
Misfit99 <amansabale007@gmail.com>
2022-04-29 00:10:14 +0530

    Test Error Solved

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -2,7 +2,7 @@ const request = require('supertest');
 const app = require('../../app');
 const { loadPlanetsData } = require('../../model/planets.model');
 const {mongoConnect, mongoDisconnect} = require('../../services/mongo');
-const { loadPlanetsData } = require('../../model/planets.model');
+
 
 
 describe('Testing Launches Api', () => {
